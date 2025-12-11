a804789e342a4199749d24caa7250958d780c904
ilyas2468 <ilyas.siddiqui2001@gmail.com>
2025-12-11 02:42:23 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/reminders/FajrTasks.java
@@ -0,0 +1,40 @@
+package com.example.reminders;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toolbar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Objects;
+
+public class FajrTasks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_fajr_tasks);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void goBack(View v){
+        //go back to main
+        Intent f = new Intent(this, MainActivity.class);
+        startActivity(f);
+    }
+
+    public void newTask(View view) {
+    }
+}
